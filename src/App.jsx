8f93c62a5586b3d1a32d7734653534cc56393442
Nirmal-Kharal <nirmalkharal40@gmail.com>
2025-07-14 09:40:05 +0545

    add main components: NavBar, Hero, and ComingSoon; implement useMaskSettings for responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
+import NavBar from './sections/NavBar';
+import Hero from './sections/Hero';
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
    <main>
-    <nav>
-        <img className='scale-125' src="./images/nav-logo.svg" alt="nav-logo" />
-        <img className='w-10' src="./images/menu.svg" alt="menu" />
-    </nav>
+    <NavBar/>
+    <Hero/>
+    
    </main>
   )
 }
