7da39c09793b75a44d42cf0e2f6107eb938963cc
Nirmal-Kharal <nirmalkharal40@gmail.com>
2025-07-22 21:06:20 +0545

    add Outro component to App; integrate into main layout

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import SecondVideo from './sections/SecondVideo';
 import Lucia from './sections/Lucia';
 import PostCard from './sections/PostCard';
 import Final from './sections/Final';
+import Outro from './sections/Outro';
 
 
 gsap.registerPlugin(ScrollTrigger);
@@ -23,6 +24,7 @@ const App = () => {
     <Lucia />
     <PostCard/>
     <Final />
+    <Outro />
     
    </main>
   )
