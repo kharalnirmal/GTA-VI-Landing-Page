9776900b40fd912a1d279d869fee598e60e06bd3
Nirmal-Kharal <nirmalkharal40@gmail.com>
2025-07-22 21:41:32 +0545

    refactor Final component; uncomment gsap.timeline for scroll animations

--- src/sections/Final.jsx
+++ src/sections/Final.jsx
@@ -8,15 +8,15 @@ const Final = () => {
   useGSAP(() => {
     gsap.set('.final-content', { opacity: 0 });
 
-    // gsap.timeline({
-    //   scrollTrigger: {
-    //     trigger: '.final',
-    //     start: 'top top',
-    //     end: '90% top',
-    //     scrub: true,
-    //     pin: true,
-    //   }
-    // })
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: '.final',
+        start: 'top top',
+        end: '90% top',
+        scrub: true,
+        pin: true,
+      }
+    })
 
     const tl = gsap.timeline({ 
       scrollTrigger: {
@@ -27,9 +27,7 @@ const Final = () => {
         pin: true,
       }
     })
-
     tl.to('.final-content', { opacity: 1, duration: 1, scale: 1, ease: 'power1.inOut' });
-
     videoRef.current.onloadedmetadata = () => {
       tl.to(videoRef.current, { currentTime: videoRef.current.duration, duration: 3, ease: 'power1.inOut' }, '<');
     }
